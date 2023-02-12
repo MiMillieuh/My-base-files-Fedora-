e4207ddba6c9063b28c233e8a7be06c5e0023d36
MiMillieuh <52078885+MiMillieuh@users.noreply.github.com>
2023-02-12 14:41:08 +0100

    Update baseinstallnonroot.sh

--- baseinstallnonroot.sh
+++ baseinstallnonroot.sh
@@ -1,4 +1,9 @@
 cargo install starship --locked
+
+
+rm -f $HOME/.config/starship.toml
+curl -L https://raw.githubusercontent.com/MiMillieuh/My-base-files-Fedora-/main/Files/Starship/starship.toml -o $HOME/.config/starship.toml
+
 gsettings set org.gnome.desktop.wm.preferences button-layout ":minimize,maximize,close"
 
 gsettings set org.gnome.desktop.interface color-scheme 'prefer-dark'
